80d3324ff62dc2aa3d40a2adcd8000c125bf80e7
mohammad shourabi <mohammad.sh.1370@gmail.com>
2024-05-27 13:51:08 +0330

    delete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -14,10 +14,30 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
-
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if (request.method == 'POST'):
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There is an error adding a task'
+    else:
+        tasks = Todo.query.order_by(Todo.data_created).all()
+        return render_template("index.html", tasks=tasks)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There is an error deleting the task'
 
 if __name__ == "__main__":
     app.run(debug=True)
